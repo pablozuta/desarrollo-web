68ce87b9c96c3892f80f36a91ac3ed40607b8f72
pablozuta <pablo.zuta.h@gmail.com>
2023-04-18 04:28:11 -0400

    se separa pagina principal en componentes

---
+++ src/routes/Form.svelte
@@ -0,0 +1,217 @@
+<script>
+  import lamp from './assets/lamp.png'
+  import '../app.css';
+</script>
+
+<section
+    aria-labelledby="contact"
+    class="container mx-auto px-8 overflow-hidden"
+  >
+    <div class="flex flex-wrap justify-center gap-12 md:gap-6 lg:gap-20">
+      <div class="md:flex-1 md:max-w-sm relative">
+        <img src={lamp} alt="Lamp" class="mx-auto mt-20" />
+        
+        
+        <div
+          class="
+            absolute
+            -bottom-18
+            left-1/2
+            -translate-x-1/2
+            -z-10
+            aspect-square
+            md:border-8
+            border-amber-400
+            rounded-full
+            md:w-72
+            lg:w-96
+          "
+        />
+      </div>
+      <form
+        class="
+          relative
+          border-8
+          border-neutral-900
+          p-6
+          rounded-lg
+          grid
+          gap-8
+          md:flex-1
+          md:max-w-lg
+          my-4
+          md:my-12
+          lg:my-16
+          bg-white
+          dark:bg-neutral-800
+          w-full
+      "
+      >
+        <h2 id="contact" class="text-3xl font-bold">Contactanos</h2>
+        <div class="relative">
+          <input
+            type="text"
+            id="name"
+            class="
+          peer
+          form-input
+          w-full
+          border-4
+          border-amber-400
+          rounded-md
+          focus:ring-4
+          dark:focus:ring-offset-2
+          focus:ring-amber-400
+          focus:border-amber-400
+          focus:outline-none
+          dark:bg-amber-400
+          dark:text-neutral-900
+          placeholder-transparent
+      "
+            placeholder="Your Name"
+          />
+          <label
+            for="name"
+            class="
+        text-neutral-500
+        text-sm
+        font-bold
+        uppercase
+        absolute
+        -top-4
+        left-2
+        -translate-y-1/2
+        transition-all
+        peer-placeholder-shown:left-4
+        peer-placeholder-shown:top-1/2
+        peer-placeholder-shown:text-neutral-900
+        peer-focus:-top-4
+        peer-focus:left-2
+        peer-focus:text-neutral-600
+        dark:peer-focus:text-neutral-300
+      "
+          >
+            Nombre
+          </label>
+        </div>
+        <div class="relative">
+          <input
+            type="email"
+            id="email"
+            class="
+          peer
+          form-input
+          w-full
+          border-4
+          border-amber-400
+          rounded-md
+          focus:ring-4
+          dark:focus:ring-offset-2
+          focus:ring-amber-400
+          focus:border-amber-400
+          focus:outline-none
+          dark:bg-amber-400
+          dark:text-neutral-900
+          placeholder-transparent
+        "
+            placeholder="Your Email"
+          />
+          <label
+            for="email"
+            class="
+        text-neutral-500
+        text-sm
+        font-bold
+        uppercase
+        absolute
+        -top-4
+        left-2
+        -translate-y-1/2
+        transition-all
+        peer-placeholder-shown:left-4
+        peer-placeholder-shown:top-1/2
+        peer-placeholder-shown:text-neutral-900
+        peer-focus:-top-4
+        peer-focus:left-2
+        peer-focus:text-neutral-600
+        dark:peer-focus:text-neutral-300
+      "
+          >
+            Email
+          </label>
+        </div>
+        <div class="relative">
+          <textarea
+            name="content"
+            id="content"
+            cols="20"
+            rows="5"
+            class="
+        peer
+        form-textarea
+        resize-none
+        w-full
+        border-4
+        border-amber-400
+        rounded-md
+        focus:ring-4
+        dark:focus:ring-offset-2
+        focus:ring-amber-400
+        focus:border-amber-400
+        focus:outline-none
+        dark:bg-amber-400
+        dark:text-neutral-900
+        placeholder-transparent
+      "
+            placeholder="How can we help?"
+          />
+          <label
+            for="content"
+            class="
+          text-neutral-500
+          text-sm
+          font-bold
+          uppercase
+          absolute
+          -top-3
+          left-2
+          -translate-y-1/2
+          transition-all
+          peer-placeholder-shown:left-4
+          peer-placeholder-shown:top-6
+          peer-placeholder-shown:text-neutral-900
+          peer-focus:-top-4
+          peer-focus:left-2
+          peer-focus:text-neutral-600
+          dark:peer-focus:text-neutral-300
+        "
+          >
+            Como Podemos Ayudarte?
+          </label>
+        </div>
+        <a
+          role="menuitem"
+          class="
+        py-2
+        px-6
+        bg-neutral-900
+        text-white
+        w-max
+        shadow-xl
+        hover:shadow-none
+        transition-shadow
+        focus:outline-none
+        focus-visible:ring-4
+        ring-neutral-900
+        rounded-md
+        ring-offset-4
+        ring-offset-white
+        dark:ring-offset-amber-400
+      "
+          href="/"
+        >
+          Enviar
+        </a>
+      </form>
+    </div>
+  </section>
---
+++ src/routes/Partners.svelte
@@ -0,0 +1,69 @@
+<section
+      aria-labelledby="partners"
+      class="text-center grid gap-8 place-items-center"
+    >
+      <div class="grid gap-4">
+        <h2 id="partners" class="text-4xl font-bold text-amber-400">
+          Satisfacción.
+        </h2>
+        <p class="w-full max-w-lg">
+          El éxito de nuestros clientes es nuestra mayor satisfacción. Nos
+          esforzamos por superar sus expectativas y brindar soluciones
+          excepcionales.
+        </p>
+      </div>
+
+      <div
+        class="flex flex-wrap justify-center gap-8 md:gap-x-16 max-w-2xl mx-auto"
+      >
+        <div class="p-4 bg-amber-400 shadow-md dark:shadow-xl rounded-md">
+          <img
+            src="https://www.svgrepo.com/show/227480/design-skills-ruler.svg"
+            alt="Partner"
+            class="h-16 w-16"
+          />
+        </div>
+        <div class="p-4 bg-amber-300 shadow-md dark:shadow-xl rounded-md">
+          <img
+            src="https://www.svgrepo.com/show/227471/pantone-color-palette.svg"
+            alt="Partner"
+            class="h-16 w-16"
+          />
+        </div>
+        <div class="p-4 bg-amber-400 shadow-md dark:shadow-xl rounded-md">
+          <img
+            src="https://www.svgrepo.com/show/227484/browser-ui.svg"
+            alt="Partner"
+            class="h-16 w-16"
+          />
+        </div>
+        <div class="p-4 bg-amber-300 shadow-md dark:shadow-xl rounded-md">
+          <img
+            src="https://www.svgrepo.com/show/227477/browser-ui.svg"
+            alt="Partner"
+            class="h-16 w-16"
+          />
+        </div>
+        <div class="p-4 bg-amber-500 shadow-md dark:shadow-xl rounded-md">
+          <img
+            src="https://www.svgrepo.com/show/227486/browser-ui.svg"
+            alt="Partner"
+            class="h-16 w-16"
+          />
+        </div>
+        <div class="p-4 bg-amber-400 shadow-md dark:shadow-xl rounded-md">
+          <img
+            src="https://www.svgrepo.com/show/227475/color-palette.svg"
+            alt="Partner"
+            class="h-16 w-16"
+          />
+        </div>
+        <div class="p-4 bg-amber-500 shadow-md dark:shadow-xl rounded-md">
+          <img
+            src="https://www.svgrepo.com/show/227482/paint-art.svg"
+            alt="Partner"
+            class="h-16 w-16"
+          />
+        </div>
+      </div>
+    </section>
